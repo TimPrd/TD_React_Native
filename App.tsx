a6ec89ff3826649bc32dc39bb96b2c0a58338eaa
TimPrd <timothe.pardieu@outlook.com>
2020-05-21 01:12:27 +0200

    update project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,88 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'native-base';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import DetailScreen from './src/views/DetailsScreen'
+
+import HistoryScreen from './src/views/HistoryScreen'
+import ScanScreen from './src/views/ScanScreen'
+import ResultScreen from './src/views/ResultScreen'
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { AsyncStorage } from 'react-native';
+
+
+
+function ScanStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Scanner" component={ScanScreen}></Stack.Screen>
+      <Stack.Screen name="Details" component={DetailScreen}></Stack.Screen>
+    </Stack.Navigator>
   );
 }
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    //AsyncStorage.clear();
+
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              switch (route.name) {
+                case 'History':
+                  iconName = 'hourglass';
+                  break;
+                case 'Scanner':
+                  iconName = 'barcode';
+                  break;
+                case 'Results':
+                  iconName = 'analytics';
+                  break;
+                default: iconName = 'home'
+              }
+              return <Icon name={iconName} style={{ fontSize: size, color }} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Scanner" component={ScanStack} />
+          <Tab.Screen name="History" component={HistoryScreen} />
+          <Tab.Screen name="Results" component={ResultScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
